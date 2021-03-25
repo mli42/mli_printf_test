6b5e045179c7770cfe1c32d2a4bc024583f635bc
Marc <marc.li5101@gmail.com>
2021-03-25 14:26:18 +0100

    ./do.sh clean (+) delete true_one.c

--- do.sh
+++ do.sh
@@ -7,11 +7,18 @@ printf_a="$libftprintf_path/$libftprintf_file"
 srcs=(test.c)
 true_file="true.txt"
 this_file="this.txt"
+bin_="a.out"
 
 function ft_compile () {
-	gcc -Wall -Wextra -Werror $srcs $printf_a -DFT_PRINTF=$1
-	./a.out | cat -e > $2
+	gcc -Wall -Wextra -Werror $srcs $printf_a -DFT_PRINTF=$1 -o $bin_
+	./$bin_ | cat -e > $2
 }
+
+if [ "$1" == "clean" ]; then
+	rm -f $true_file $this_file $bin_
+	echo "Trailing files cleaned!"
+	exit
+fi
 
 make -C $libftprintf_path fclean && make -C $libftprintf_path -j
 
